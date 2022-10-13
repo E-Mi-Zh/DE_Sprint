444007596ec78e7040a9b43876fcabff565974e6
E-Mi-Zh <103973578+E-Mi-Zh@users.noreply.github.com>
2022-10-14 00:41:36 +1000

    Update palindrome.py
    Changed output type from str to boolean

--- task_1.3/palindrome.py
+++ task_1.3/palindrome.py
@@ -6,6 +6,6 @@ rev = inp[::-1]
 rev_clean = rev.replace(" ", "")
 print(rev)
 if (inp_clean == rev_clean):
-    print(f"{inp} = {rev} - палиндром!")
+    print(True)
 else:
-    print(f"{inp} != {rev} - не палиндром.")
+    print(False)
